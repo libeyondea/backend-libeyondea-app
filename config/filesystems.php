cf4a6b53989e88f149a3e845ba59f66a10f9c0f9
libeyondea <57558120+libeyondea@users.noreply.github.com>
2022-05-25 09:04:45 +0700

    up

--- config/filesystems.php
+++ config/filesystems.php
@@ -2,7 +2,7 @@
 
 return [
 
-    /*
+	/*
     |--------------------------------------------------------------------------
     | Default Filesystem Disk
     |--------------------------------------------------------------------------
@@ -13,9 +13,9 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DRIVER', 'local'),
+	'default' => env('FILESYSTEM_DRIVER', 'local'),
 
-    /*
+	/*
     |--------------------------------------------------------------------------
     | Filesystem Disks
     |--------------------------------------------------------------------------
@@ -28,39 +28,39 @@ return [
     |
     */
 
-    'disks' => [
+	'disks' => [
 
-        'local' => [
-            'driver' => 'local',
-            'root' => storage_path('app'),
-        ],
+		'local' => [
+			'driver' => 'local',
+			'root' => storage_path('app'),
+		],
 
-        'public' => [
-            'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL') . '/storage',
-            'visibility' => 'public',
-        ],
+		'public' => [
+			'driver' => 'local',
+			'root' => storage_path('app/public'),
+			'url' => env('APP_URL') . '/storage',
+			'visibility' => 'public',
+		],
 
-        's3' => [
-            'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-        ],
+		's3' => [
+			'driver' => 's3',
+			'key' => env('AWS_ACCESS_KEY_ID'),
+			'secret' => env('AWS_SECRET_ACCESS_KEY'),
+			'region' => env('AWS_DEFAULT_REGION'),
+			'bucket' => env('AWS_BUCKET'),
+			'url' => env('AWS_URL'),
+			'endpoint' => env('AWS_ENDPOINT'),
+			'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+		],
 
-        'img' => [
-            'driver' => 'local',
-            'root' => public_path(env('IMG_FOLDER', 'images'))
-        ],
+		'img' => [
+			'driver' => 'local',
+			'root' => public_path(env('IMG_FOLDER', 'images'))
+		],
 
-    ],
+	],
 
-    /*
+	/*
     |--------------------------------------------------------------------------
     | Symbolic Links
     |--------------------------------------------------------------------------
@@ -71,8 +71,8 @@ return [
     |
     */
 
-    'links' => [
-        public_path('storage') => storage_path('app/public'),
-    ],
+	'links' => [
+		public_path('storage') => storage_path('app/public'),
+	],
 
 ];
