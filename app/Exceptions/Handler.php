ba6a5579a4655498e16501e4f72edb75cffd8f59
libeyondea <57558120+libeyondea@users.noreply.github.com>
2022-06-01 00:25:03 +0700

    update

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,9 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Arr;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -37,5 +39,42 @@ class Handler extends ExceptionHandler
 		$this->reportable(function (Throwable $e) {
 			//
 		});
+	}
+
+	/**
+	 * Convert an authentication exception into a response.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Illuminate\Auth\AuthenticationException  $exception
+	 * @return \Symfony\Component\HttpFoundation\Response
+	 */
+	protected function unauthenticated($request, AuthenticationException $exception)
+	{
+		return $this->shouldReturnJson($request, $exception)
+			? response()->json(['message' => $exception->getMessage(), 'errors' => null], 401)
+			: redirect()->guest($exception->redirectTo() ?? route('login'));
+	}
+
+	/**
+	 * Convert the given exception to an array.
+	 *
+	 * @param  \Throwable  $e
+	 * @return array
+	 */
+	protected function convertExceptionToArray(Throwable $e)
+	{
+		return config('app.debug') ? [
+			'message' => $e->getMessage(),
+			'errors' => null,
+			'exception' => get_class($e),
+			'file' => $e->getFile(),
+			'line' => $e->getLine(),
+			'trace' => collect($e->getTrace())->map(function ($trace) {
+				return Arr::except($trace, ['args']);
+			})->all(),
+		] : [
+			'message' => $this->isHttpException($e) ? $e->getMessage() : 'Server Error',
+			'errors' => null
+		];
 	}
 }
