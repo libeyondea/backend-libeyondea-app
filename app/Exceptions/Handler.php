313256908b48aa51b70f0a75a840d91c80896838
Libeyondea <57558120+libeyondea@users.noreply.github.com>
2023-02-04 22:28:20 +0700

    Update handle error

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Support\Arr;
 use Throwable;
@@ -43,6 +44,27 @@ class Handler extends ExceptionHandler
 		$this->reportable(function (Throwable $e) {
 			//
 		});
+	}
+
+	/**
+	 * Convert an authentication exception into a response.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Illuminate\Auth\AuthenticationException  $exception
+	 * @return \Symfony\Component\HttpFoundation\Response
+	 */
+	protected function unauthenticated($request, AuthenticationException $exception)
+	{
+		return $this->shouldReturnJson($request, $exception)
+			? response()->json(
+				[
+					'success' => false,
+					'code' => 401,
+					'message' => $exception->getMessage(),
+				],
+				401
+			)
+			: redirect()->guest($exception->redirectTo() ?? route('login'));
 	}
 
 	/**
